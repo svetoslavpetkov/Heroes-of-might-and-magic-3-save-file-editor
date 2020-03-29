24a99109517a7925da1685800a90ceb64a39ec26
Svetoslav Petkov <svetoslav.petkov@gmail.com>
2020-03-29 02:06:11 +0200

    parse secondary skills and spells

--- electron/adapters/defaultAdapter.ts
+++ electron/adapters/defaultAdapter.ts
@@ -1,13 +1,12 @@
 import { IFileAdapter } from "./abstraction";
-import { Map, PrimarySkills, Hero } from "../model/model"
+import { Map, PrimarySkills, Hero, Position, OwnedCreature, SecondarySkillLevel, SecondarySkill, LearnedSecondarySkill, Creature } from "../model/model"
 import { saveTextFile } from "../file-util"
 
 export class DefaultAdapter implements IFileAdapter {
 
     getHeroNames(): Array<string> {
-
-        return ["Solmyr", "Fafner", "Daremyth"];
-       /* return ["Orrin",
+        //return ["Solmyr", "Fafner", "Daremyth"];
+       return ["Orrin",
         "Valeska",
         "Edric",
         "Sylvia",
@@ -167,7 +166,10 @@ export class DefaultAdapter implements IFileAdapter {
         "Zilare",
         "Astra",
         "Dargem",
-        "Giselle"];*/
+        "Giselle",
+        //campaign heroes
+        "Xeron",
+        ];
     }
 
     readData(buffer:Buffer): Map {
@@ -177,31 +179,98 @@ export class DefaultAdapter implements IFileAdapter {
         console.log("start searching for heroes");
 
         const heroes:Array<Hero> = [];
-        this.getHeroNames().forEach(heroName => { 
+        this.getHeroNames().forEach(heroName => {
+            const tempHeroes: Array<Hero> = [];
             let indexOf = 0;
             while (indexOf >= 0) {
-                indexOf = text.indexOf(heroName, indexOf + 20);
+                indexOf = text.indexOf(heroName.padEnd(13, "\0"), indexOf + 20);
                 if (indexOf > 0) {
                     const hero = this.readHero(indexOf, heroName, buffer);
-                    heroes.push(hero);
+                    tempHeroes.push(hero);
                 } else {
                     break;
                 }
             }
+
+            if (tempHeroes.length > 0) {
+                //we always add the last hero
+                heroes.push(tempHeroes[tempHeroes.length-1]);
+            }
         });
 
-        
-
-        console.log(JSON.stringify(heroes));
+        console.log(JSON.stringify(heroes.filter(h => h.position.surfaceIndex <= 1)));
 
         return new Map();
     }
 
-
     readHero(heroIndex:number,heroName: string ,buffer:Buffer): Hero {
         const primarySkills = this.readPrimarySkills(heroIndex, buffer);
-        
-        return new Hero(heroIndex, heroName, primarySkills);
+        const position = this.readPosition(heroIndex, buffer);
+        const secondarySkiils = this.readSecondarySkills(heroIndex, buffer);
+        const creatures = this.readCreatures(heroIndex, buffer);
+        return new Hero(heroIndex, heroName, primarySkills, position, secondarySkiils, creatures);
+    }
+
+    readCoordinate(index1: number, index2: number, buffer: Buffer):number {
+        return buffer.readInt16LE(index1);
+    }
+
+    readSecondarySkills(heroIndex: number, buffer: Buffer):LearnedSecondarySkill[] {
+        const tempResult: LearnedSecondarySkill[] = [];
+        const skillsLevelOffset = +13;
+        for (let index = 0; index < 28; index++) {
+            if(buffer[heroIndex + skillsLevelOffset + index] > 0) {
+                tempResult.push({
+                    level: buffer[heroIndex + skillsLevelOffset + index],
+                    skill: index,
+                });
+            }
+        }
+
+        const skillPosittionOffsett = +41;
+        const orderedResult: LearnedSecondarySkill[] = new Array(tempResult.length);
+        tempResult.forEach(learnedSkill => {
+            const skillIndex = buffer[heroIndex + skillPosittionOffsett + learnedSkill.skill];
+            orderedResult[skillIndex-1] = learnedSkill;
+        });
+        return orderedResult;
+    } 
+
+    readCreatures(heroIndex: number, buffer: Buffer):OwnedCreature[] {
+        const creatureIdOffset = -56;
+        const creatureAmoutnOffset = -28;
+
+        return [0,1,2,3,4,5,6].map( index => {
+            const creatureId = buffer[heroIndex + creatureIdOffset + index*4];
+            const creature: Creature = (creatureId === 255) ? Creature.NoCreature : creatureId;
+
+            let quantity = 0;
+
+            if(creatureId !== Creature.NoCreature) {
+                quantity = buffer.readInt16LE(heroIndex + creatureAmoutnOffset + index*4);
+            }
+
+            return { 
+                creature,
+                quantity
+            }
+        });
+    }
+
+    readPosition(heroIndex: number, buffer: Buffer): Position {
+       const surfaceData = buffer[heroIndex - 191];
+       if (surfaceData < 0 || 1 < surfaceData) {
+        console.error(`Hero index ${heroIndex} invalid surface value: ${surfaceData}`);
+       }
+       else {
+           console.log(`Hero index ${heroIndex} has VALID  surface value: ${surfaceData}`)
+       }
+       return {
+           X: this.readCoordinate(heroIndex - 195, heroIndex - 194, buffer),
+           Y: this.readCoordinate(heroIndex - 193, heroIndex - 192, buffer),
+           surfaceIndex: surfaceData,
+           isOnSurface: surfaceData === 0,
+       }
     }
 
 
--- electron/model/model.ts
+++ electron/model/model.ts
@@ -6,6 +6,132 @@ export interface PrimarySkills {
     Knowledge: Number,
 }
 
+export enum Creature {
+    NoCreature = 0,
+Pikeman,
+Halberdier,
+Archer,
+Marksman,
+Griffin,
+RoyalGriffin,
+Swordsman,
+Crusader,
+Monk,
+Zealot,
+Cavalier,
+Champion,
+Angel,
+Archangel,
+Centaur,
+CentaurCapitan,
+Dwarf,
+BattleDwarf,
+WoodElf,
+GrandElf,
+Pegasus,
+SilverPegasus,
+DendroidGuard,
+DendroidSolider,
+Unicorn,
+WarUnicorn,
+GreenDragon,
+GoldDragon,
+Gremlin,
+MasterGremlin,
+StoneGargoyle,
+ObsidianGargoyle,
+StoneGolem,
+IronGolem,
+Mage,
+ArchMage,
+Genie,
+MasterGenie,
+Naga,
+NagaQueen,
+Giant,
+Titan,
+Imp,
+Familiar,
+Gog,
+Magog,
+HellHound,
+Cerberus,
+Daemon,
+HornedDaemon,
+PitFiend,
+PitLord,
+Efreet,
+EfreetSultan,
+Devil,
+ArchDevil,
+Skeleton,
+SkeletonWarrior,
+WalkingDead,
+Zombie,
+Wight,
+Wraight,
+Vampire,
+VampireLord,
+Lich,
+PowerLich,
+BlackKnight,
+DeadKnight,
+BoneDragon,
+GhostDragon,
+Tryglodyte,
+InfernalTryglodyte,
+Harpy,
+HarpyHag,
+Beholder,
+EvilEye,
+Medusa,
+MedusaQueen,
+Minotaur,
+MinotaurKing,
+Manticore,
+Scorpicore,
+RedDragon,
+BlackDragon,
+Goblin,
+Hobgoblin,
+WolfRider,
+WolfRaider,
+Orc,
+OrcChieftain,
+Ogre,
+OgreMage,
+Roc,
+Thunderbird,
+Cyclops,
+CyclopsKing,
+Behemoth,
+AncientBehemoth,
+Gnoll,
+GnollMarauder,
+Lizardman,
+LizardWarrior,
+SerpentFly,
+Dragonfly,
+Basilisk,
+GreaterBasilisk,
+Gorgon,
+MightyGorgon,
+Wyvern,
+WyvernMonarch,
+Hydra,
+ChaosHydra,
+AirElemental,
+EarthElemental,
+FireElemental,
+WaterElemental,
+Golem,
+DiamondGolem,
+Pixies,
+Sprites,
+PsychicElementals,
+MagicElementals,
+}
+
 export enum SecondarySkill {
     Pathfinding = 0,
     Archery,
@@ -43,14 +169,37 @@ export enum SecondarySkillLevel {
     Expert = 3,
 }
 
+export interface LearnedSecondarySkill {
+    skill: SecondarySkill,
+    level: SecondarySkillLevel,
+}
+
+export interface Position {
+    X: number,
+    Y: number,
+    surfaceIndex: number,
+    isOnSurface: boolean
+}
+
+export interface OwnedCreature {
+    creature: Creature,
+    quantity: number,
+}
 export class Hero {
     startingIndex:number;
     name:string;
     primarySkills:PrimarySkills;
-    constructor(startingIndex:number,name:string, primarySkills:PrimarySkills) {
+    secondarySkills:LearnedSecondarySkill[];
+    creatures: OwnedCreature[];
+    position: Position;
+    constructor(startingIndex:number,name:string, primarySkills:PrimarySkills, position: Position,
+        secondarySkills:LearnedSecondarySkill[], creatures: OwnedCreature[]) {
         this.startingIndex = startingIndex;
         this.name = name;
         this.primarySkills = primarySkills;
+        this.position = position;
+        this.secondarySkills = secondarySkills;
+        this.creatures = creatures;
     }
 }
 
